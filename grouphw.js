c71e5dcc33f16dc95144a1359b033205a71468e7
trentashford27 <trentashford@oru.edu>
2025-10-01 13:11:14 -0500

    Add files via upload

---
+++ grouphw.js
@@ -0,0 +1,33 @@
+// ============================================
+// CSC 111 – Introduction to Computing
+// Assignment: [Sequences, Selections, Loops]
+// Student Name: [Trenton Ashford]
+// Z Number: Z60672398
+// Date: [2025-10-01]
+// Instructor: Prof. Felicity M. Weed Jackson
+// File Name: grouphw.js <------ (change this based on assignment instructions!)
+// ============================================
+/* Description:
+This program [briefly describe what the program does].
+It uses [mention key JavaScript concepts used, e.g., variables, loops, functions].
+*/
+
+/* How it works:
+[Explain the logic or flow of the program in simple terms.
+For example: "The program asks the user for ___ input, performs a ____ calculation,
+and then displays the result in the console or on the webpage."]
+*/
+
+/* Reflection:
+[Write 2–3 sentences about what you learned, what was challenging,
+or what you found interesting about this assignment.]
+*/
+// ============================================
+
+// [start coding here]
+
+// Sequence Code in Javascript
+for (let x = 1; x <= 10; x++){
+    console.log(x);
+}
+
---
+++ grouphw.py
@@ -0,0 +1,31 @@
+# ============================================
+# CSC 111 – Introduction to Computing
+# Lab #: [Sequences, Selections, Loops]
+# Student Name: [Trenton Ashford]
+# Z Number: Z60672398
+# Date: [2025-10-01]
+# Instructor: Prof. Felicity M. Weed Jackson
+# File Name: grouphw.py <------ (change this based on lab instructions!)
+# ============================================
+
+# Description:
+# This program [briefly describe what the program does].
+# It uses [mention key Python concepts used, e.g., variables, loops, functions].
+#
+# How it works:
+# [Explain the logic or flow of the program in simple terms.
+# For example: "The program asks the user for ___ input, performs a ____ calculation,
+# and then prints the result."]
+
+# Reflection:
+# [Write 2–3 sentences about what you learned, what was challenging,
+# or what you found interesting about this lab.]
+
+# ============================================
+
+# [start coding here]
+
+# Sequence code in Python
+sequence = [4 + x * 4 for x in range(6)]
+print(sequence)
+
